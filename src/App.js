81b5b3896f20c21e3c2eada8048a3883da2b0117
John Philip <developerphilo@gmail.com>
2020-05-22 10:49:55 +0100

    get informatio about countries

--- src/App.js
+++ src/App.js
@@ -16,6 +16,7 @@ import './App.css';
                       fetch(`https://restcountries.eu/rest/v2/name/${state.search}?fullText=true`)
                       .then(response => response.json())
                       .then(data=>{
+                        console.log(data[0]);
                         setState(prevState =>{
                           return {...prevState, country: data[0]}
                         });
@@ -37,12 +38,19 @@ import './App.css';
       <Navbar handleInput={handleInput} getSearch={getSearch}/>
           <div className="country">
               <div>
-                  <h1>{ state.country.demonym === "" ? state.search  : state.country.demonym }</h1>
+                  <h1>Demonym : { state.country.demonym === "" ? state.search  : state.country.demonym }</h1>
+                  <h2>Native Name : {state.country.nativeName}</h2>
                   <h3>Population {state.country.population}</h3>
                   <p>Subregion : {state.country.subregion}</p>
                   <p>Capital : {state.country.capital}</p>
                   <p>Numeric Code : {state.country.numericCode}</p>
                   <p>Region : {state.country.region}</p>
+                  {/* <p>Currency Name : {state.country.currencies.name}</p> */}
+                  <p>Cioc : {state.country.cioc}</p>
+                  <p>Calling Code : {state.country.callingCodes}</p>
+                  <p>Timezone: {state.country.timezones}</p>
+                  <p>Domain: {state.country.topLevelDomain}</p>
+                  <p>Area: {state.country.area}</p>
               </div>
               <div>
                   <img src={state.country.flag} alt="Logo" />
